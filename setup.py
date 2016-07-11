e81bce7bdf48965161aa8882f23e358a91acfc07
uryoya <uryoya.ur@gmail.com>
2016-07-12 03:22:39 +0900

    依存関係を記述した

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 setup(name='burstditector',
       version='0.1.0',
       py_modules=['burstditector'],
+      install_requires=[
+          "numpy",
+          "scipy",
+      ]
       )
